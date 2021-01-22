85fd206a4d970f36baa156bba789575cf78114f8
Peter Kingston <contactme@peterjkingston.com>
2021-01-22 12:30:44 -0800

    Test Lib added for QuickEncryptLib

---
+++ QuickEncryptLib_Test/EncryptionService_Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickEncrypt.Encryption;
+using System;
+
+namespace QuickEncryptLib_Test
+{
+	[TestClass]
+	public class EncryptionService_Test
+	{
+		EncryptionService _encryptionService = new EncryptionService();
+
+		[TestMethod]
+		public void WritesKey_WhenNoKeyFound()
+		{
+			throw new Exception();
+		}
+
+
+	}
+}
---
+++ QuickEncryptLib_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuickEncryptLib_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuickEncryptLib_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e83dc23-c89b-4463-a9fb-92ed526884ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
