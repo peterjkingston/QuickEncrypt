eba3d24ad98ee0f20c1eb8474d4781ea8c64e89c
Peter Kingston <46848056+peterjkingston@users.noreply.github.com>
2021-02-01 18:05:49 -0800

    Basic MainWindow layout added, nothing connected yet. MainWindow interfaces IApplicationViewPort.

---
+++ QuickEncrypt/ViewPorts/IApplicationViewport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickEncrypt.ViewPorts
+{
+    public interface IApplicationViewport
+    {
+        void DisplayFile();
+    }
+}
